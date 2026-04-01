877a6b2ed38176f28cad80fc1832a1c7690fafab
Samiksha Sengar <samikshasengar59@gmail.com>
2026-04-01 20:34:20 +0530

    Create app.py

---
+++ server/app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import inference
+
+app = FastAPI()
+
+
+class InputData(BaseModel):
+    resume_text: str
+    job_description: str
+
+
+@app.get("/")
+def home():
+    return {"message": "OpenEnv API Running"}
+
+
+@app.post("/reset")
+def reset():
+    return inference.reset_env()
+
+
+@app.post("/predict")
+def predict(data: InputData):
+    return inference.predict(data.dict())
