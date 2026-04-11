c490fe94dee67ff9d0b40f6a1bb0f957912230ec
Samiksha Sengar <samikshasengar59@gmail.com>
2026-04-11 23:02:37 +0530

    fixed scorer merge conflict

--- scorer.py
+++ scorer.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def calculate_score(matched, missing, similarity):
 
     skill_score = (len(matched) / (len(matched) + len(missing) + 1)) * 60
@@ -7,7 +7,6 @@ def calculate_score(matched, missing, similarity):
 
     total_score = skill_score + similarity_score
 
-=======
 def calculate_score(matched, missing, similarity):
 
     skill_score = (len(matched) / (len(matched) + len(missing) + 1)) * 60
@@ -16,5 +15,5 @@ def calculate_score(matched, missing, similarity):
 
     total_score = skill_score + similarity_score
 
->>>>>>> d89e674387131ac4a6df1aba512450ec1da38eb1
+
     return round(total_score, 2)
